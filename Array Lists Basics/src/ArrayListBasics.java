4be588e758b91dc160abcba74450f73da5584476
ethanwalters <ethanwalters@ethans-mbp.hsd1.co.comcast.net>
2017-09-13 17:08:31 -0600

    challenge five not working

--- Array Lists Basics/src/ArrayListBasics.java
+++ Array Lists Basics/src/ArrayListBasics.java
@@ -4,9 +4,10 @@ public class ArrayListBasics
 	{
 public static void main(String[] args)
 	{
-		
+		// challenge one
 		ArrayList <Double> myArray = new ArrayList <Double>( );
 
+		// challenge two
 		myArray.add(99.0);
 		myArray.add(99.5);
 		myArray.add(96.3);
@@ -22,10 +23,53 @@ public static void main(String[] args)
 			
 			System.out.println(myArray.get(i) + " degrees Fahrenheit");
 		}
+		
+		// challenge three
+		int numberOfPeopleWithFev = 0;
+		
 		for(int i = 0 ; i < myArray.size() ; i++){
 			
-			if()
-			System.out.println(myArray.get(i) + " degrees Fahrenheit");
+			if(myArray.get(i) > 98.6){
+				
+			System.out.println("Uh Oh! You have a fever! Your tempature is " + myArray.get(i) + " degrees Fahrenheit.");
+			
+			numberOfPeopleWithFev = numberOfPeopleWithFev + 1;
+			}
 		}
+		
+		System.out.println("There are " + numberOfPeopleWithFev + " people with fevers.");
+		
+		// challenge four
+		
+		for(int i = 0 ; i < myArray.size() ; i++){
+			
+			double cToF = myArray.get(i);
+			
+			double subtractFunction = cToF - 32;
+			
+			double divideFunction = subtractFunction / 1.8;
+			
+			System.out.println(divideFunction + " degrees Celsius");
+			
+		}
+		// challenge five
+		
+		for(int i = 0 ; i < myArray.size() ; i++){
+			
+			if(myArray.get(i) > 98.6){
+				
+				myArray.remove(i);
+			}
+		}
+		
+		System.out.println("Healthy people:");
+		
+		for(int i = 0 ; i < myArray.size() ; i++){
+			
+			System.out.println(myArray.get(i));
+			
+		}
+			
+		
 	}
 	}
