721cbd2c404e7ff1092bd8f4c1ce66825e684ee4
Ethan.Walters <Ethan.Walters@mu-c1-03.mullen.pvt.k12.co.us>
2017-09-13 15:04:30 -0600

    initial commit

---
+++ Array Lists Basics/src/ArrayListBasics.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class ArrayListBasics
+	{
+public static void main(String[] args)
+	{
+		
+		ArrayList <Double> myArray = new ArrayList <Double>( );
+
+		myArray.add(99.0);
+		myArray.add(99.5);
+		myArray.add(96.3);
+		myArray.add(97.9);
+		myArray.add(98.4);
+		myArray.add(100.5);
+		myArray.add(101.0);
+		myArray.add(95.9);
+		myArray.add(94.3);
+		myArray.add(100.7);
+		
+		for(int i = 0 ; i < myArray.size() ; i++){
+			
+			System.out.println(myArray.get(i) + " degrees Fahrenheit");
+		}
+		for(int i = 0 ; i < myArray.size() ; i++){
+			
+			if()
+			System.out.println(myArray.get(i) + " degrees Fahrenheit");
+		}
+	}
+	}
